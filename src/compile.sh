bf2e816e356c04107058fdc7560619929cf08d15
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2014-06-23 14:50:50 +0200

    Migrate build commands to Makefile for alex/happy dependency tracking

--- src/compile.sh
+++ src/compile.sh
@@ -1,6 +1,2 @@
 #! /bin/sh
-set -e
-set -x
-alex Scanner.x
-happy FParser.y
-ghc --make -O -fglasgow-exts -fallow-overlapping-instances -fallow-undecidable-instances Main -o ../bin/pfc
+make $* ../bin/pfc
